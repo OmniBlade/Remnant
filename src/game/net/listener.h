d4abfd2cb4ce8d53d57b6270aa5c0c4b0f0e56b7
OmniBlade <nadia_xy@hotmail.com>
2020-06-19 22:39:46 +0100

    Implements ListenerClass.

---
+++ src/game/net/listener.h
@@ -0,0 +1,72 @@
+/**
+ * @file
+ *
+ * @author OmniBlade
+ *
+ * @brief Interface for protocol class implementations.
+ *
+ * @copyright Remnant is free software: you can redistribute it and/or
+ *            modify it under the terms of the GNU General Public License
+ *            as published by the Free Software Foundation, either version
+ *            2 of the License, or (at your option) any later version.
+ *            A full copy of the GNU General Public License can be found in
+ *            LICENSE
+ */
+#ifndef LISTENER_H
+#define LISTENER_H
+
+#include "always.h"
+#include "protocol.h"
+#include <sockets.h>
+
+struct DestAddress
+{
+    char Host[30];
+    unsigned short Port;
+};
+
+class ListenerProtocolClass : public ProtocolClass
+{
+public:
+    ListenerProtocolClass();
+    virtual ~ListenerProtocolClass();
+
+    virtual void Data_Received() override {}
+    virtual void Connected_To_Server(int status) override {}
+    virtual void Connection_Requested() override;
+    virtual void Closed() override {}
+    virtual void Name_Resolved() override {}
+
+private:
+    unsigned ConnectionRequests;
+};
+
+class ListenerClass
+{
+public:
+    ListenerClass();
+    ListenerClass(ProtocolClass *protocol);
+    virtual ~ListenerClass();
+
+    virtual void Register(ProtocolClass *protocol);
+    virtual int Start_Listening(void *address, int size, int unk);
+    virtual void Stop_Listening();
+
+private:
+    void Create_Window();
+    void Destroy_Window();
+    int Open_Socket(unsigned short port);
+    void Close_Socket();
+    static long __stdcall Window_Proc(HWND hwnd, unsigned umsg, unsigned wparam, long lparam);
+
+public:
+    SOCKET Socket;
+    ProtocolClass *Protocol;
+    HWND Window;
+    int IsListening : 1;
+};
+
+int Init_Listener();
+void Deinit_Listener();
+
+#endif
--- src/hooker/setupglobals.cpp
+++ src/hooker/setupglobals.cpp
@@ -4,3 +4,12 @@
 #include "alloc.h"
 memerrorhandler_t &g_MemoryError = Make_Global<memerrorhandler_t>(0x005EEA58); // Memory error handler function pointer.
 memexithandler_t &g_MemoryErrorExit = Make_Global<memexithandler_t>(0x005EEA5C);
+
+// listener.cpp
+class ProtocolClass;
+class ListenerClass;
+HINSTANCE &hWSockInstance = Make_Global<HINSTANCE>(0x005EEC10);
+ProtocolClass *&ListenerProtocol = Make_Global<ProtocolClass *>(0x00577B14);
+ListenerClass *&Listener = Make_Global<ListenerClass *>(0x00577B18);
+long &PlanetWestwoodPortNumber = Make_Global<long>(0x005346EC);
+char &GameToPlay = Make_Global<char>(0x00576AF1);
